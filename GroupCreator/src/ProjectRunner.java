9863880da4a382f3a040bde69a58f18d42bc40bc
dirkson.brinkerhoff <dirkson.brinkerhoff@MU-C1-04.mullen.pvt.k12.co.us>
2022-04-12 13:44:25 -0600

    Added pojo and filled arrayList

--- GroupCreator/src/ProjectRunner.java
+++ GroupCreator/src/ProjectRunner.java
@@ -1,11 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ProjectRunner
 	{
-
-		public static void main(String[] args)
+		// Array list
+		public static ArrayList<Student> StudentList = new ArrayList<Student>();
+		
+		
+		public static void main(String[] args) throws IOException
 			{
-				// TODO Auto-generated method stub
+				fillArrayList();
 
+			}
+		
+		public static void fillArrayList() throws IOException
+			{
+				// filling array list
+				Scanner myFile = new Scanner(new File("StudentList.txt"));
+				while (myFile.hasNext())
+					{
+						String tempArray = myFile.nextLine();
+						String[] filler = tempArray.split(" ");
+						StudentList.add(new Student(filler[0], filler[1]));
+					}
+				
+				
 			}
 
 	}
---
+++ GroupCreator/src/Student.java
@@ -0,0 +1,31 @@
+public class Student
+	{
+		private String firstName;
+		private String lastName;	
+		
+		public Student(String f, String l)
+		{
+			firstName = f;
+			lastName = l;
+		}
+		public String getFirstName()
+			{
+				return firstName;
+			}
+
+		public void setFirstName(String firstName)
+			{
+				this.firstName = firstName;
+			}
+
+		public String getLastName()
+			{
+				return lastName;
+			}
+
+		public void setLastName(String lastName)
+			{
+				this.lastName = lastName;
+			}
+		
+	}
