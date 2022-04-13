fbf6d30bb93ecee798d09c1c6403442d0c1e3a7f
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2022-04-13 13:39:51 -0600

    bad

--- GroupCreator/src/ProjectRunner.java
+++ GroupCreator/src/ProjectRunner.java
@@ -19,11 +19,13 @@ public class ProjectRunner
 			{
 				// filling array list
 				Scanner myFile = new Scanner(new File("StudentList.txt"));
-				while (myFile.hasNext())
+				int numberOfLines = myFile.nextInt();
+				
+				for(int i = 0; i < numberOfLines; i++)
 					{
 						String tempArray = myFile.nextLine();
 						String[] filler = tempArray.split(" ");
-						StudentList.add(new Student(filler[0], filler[1]));
+						StudentList.add(new Student(filler[i], filler[i + 1]));
 					}
 				// hetyyyyyy
 				
