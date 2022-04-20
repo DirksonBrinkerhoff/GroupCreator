530490168b1c977fae5b8e6c8e1a74de07e65cb5
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2022-04-20 13:46:11 -0600

    making groups

--- GroupCreator/src/ProjectRunner.java
+++ GroupCreator/src/ProjectRunner.java
@@ -7,14 +7,16 @@ public class ProjectRunner
 	{
 		// Array list
 		public static ArrayList<String> StudentList = new ArrayList<String>();
+		public static ArrayList<ArrayList<String>> Groups = new ArrayList<ArrayList<String>>();
 		// New Scanner
-		public static Scanner userStringInput = new Scanner(System.in);
-		static int countGroup = 0;
+		public static Scanner userIntInput = new Scanner(System.in);
+		static int numberOfGroups;
 		
 		public static void main(String[] args) throws IOException
 			{
 				fillArrayList();
 				promptUser();
+				newGroups();
 			}
 		
 		public static void fillArrayList() throws IOException
@@ -40,9 +42,48 @@ public class ProjectRunner
 			//Question
 			System.out.println("How many groups would you like to create?");
 			//UserInput
-			String numberOfGroups = userStringInput.nextLine();
+			int numberOfGroups = userIntInput.nextInt();
+			int groupMembers = StudentList.size() / numberOfGroups;
+			
+			for(int i = 0; i < numberOfGroups; i++)
+				{
+					ArrayList<String> TempArray = new ArrayList<String>();
+					for(int j = 0; j < groupMembers; j++)
+						{
+							TempArray.add(StudentList.get(0));
+							StudentList.remove(0);
+						}
+					Groups.add(TempArray);
+					
+				}
+			
+			for(int i = 0; i < StudentList.size(); i++)
+				{
+					for(int j = 0; j < Groups.size() && StudentList.size() > 0; j++)
+						{
+							Groups.get(j).add(StudentList.get(0));
+							StudentList.remove(0);
+						}
+				}
+			
+			for(int i = 0; i < Groups.size(); i++)
+				{
+					for(int j = 0; j < Groups.get(i).size(); j++)
+						{
+							System.out.println(Groups.get(i).get(j));
+							//System.out.print(", ");
+						}
+					
+					System.out.println();
+					
+				}
 			
 
 		}
+		
+		public static void newGroups()
+			{
+				
+			}
 
 	}
